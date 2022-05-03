25d71b673e243c72edf36b3abf40f86e1d7225ea
devPrakash7 <psamantaray77@gmail.com>
2022-05-03 13:30:51 +0530

    photo gallery

--- script.js
+++ script.js
@@ -1,31 +1,25 @@
 
-let searchForm = document.querySelector('.search-form-container');
+const person = [
+    
+    {
+       NAME:"prakash",
+      age:56,
+      weight:54,
+      height:5.9,
+      color:"black"
+    },
 
-document.querySelector('#search-btn').onclick = () =>{
-    searchForm.classList.toggle('active');
-    cart.classList.remove('active');    
-    loginForm.classList.remove('active');
-    navbar.classList.remove('active');
-}
+    {
+        NAME:"akki",
+        age:59,
+        weight:54,
+        height:5.9,
+        color:"black"
 
+    }
+]
+// name,age,weight etc is property and property value is prakash,56,64,black;
 
-let shopping = document.querySelector('.shopping-cart-container');
-
-document.querySelector('#cart-btn').onclick = () =>{
-
-    searchForm.classList.remove('active');
-    shopping.classList.toggle('active');    
-    loginForm.classList.remove('active');
-    navbar.classList.remove('active');
-}
-
-let loginForm = document.querySelector('.login-form-container');
-
-
-document.querySelector('login-btn').onclick = () =>{
-
-    searchForm.classList.remove('active');
-    shopping.classList.remove('active');    
-    loginForm.classList.toggle('active');
-    navbar.classList.remove('active');
-}
+// how to access property and value
+const object = person[1].NAME;
+console.log(object);
