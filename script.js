d9ffc000a17fd0b67798eb894760ee03dddd4756
devPrakash7 <psamantaray77@gmail.com>
2022-05-17 07:02:51 +0530

    first commit

--- script.js
+++ script.js
@@ -1,25 +1,7 @@
+const open = document.getElementById('open')
+const close = document.getElementById('close')
+const container = document.querySelector('.container')
 
-const person = [
-    
-    {
-       NAME:"prakash",
-      age:56,
-      weight:54,
-      height:5.9,
-      color:"black"
-    },
+open.addEventListener('click', () => container.classList.add('show-nav'))
 
-    {
-        NAME:"akki",
-        age:59,
-        weight:54,
-        height:5.9,
-        color:"black"
-
-    }
-]
-// name,age,weight etc is property and property value is prakash,56,64,black;
-
-// how to access property and value
-const object = person[1].NAME;
-console.log(object);
+close.addEventListener('click', () => container.classList.remove('show-nav'))
